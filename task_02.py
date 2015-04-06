d82093e6ce5433d9986c2c96e735575521e9659c
gpsingh12 <gurpreet_1441@yahoo.com>
2015-04-06 05:08:35 +0000

    NIGEL

---
+++ task_02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Accessing the data from dictionary"""
+
+import data
+NIGEL = data.BANDS['Spinal Tap']['Nigel Tufnel']
+BAND_NAMES = data.BANDS.keys()
