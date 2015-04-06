6fbbf5591205fdfe92479060121698cbb8b26e3f
gpsingh12 <gurpreet_1441@yahoo.com>
2015-04-06 05:13:25 +0000

    corrected

---
+++ task_03.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Adding and removing the entries in dictionaries"""
+import data
+CORRECTED = data.BANDS.copy()
+CORRECTED['Dylan'] = {'Bob Dylan': ['vocals', 'guitar', 'harmonica']}
+del CORRECTED['Van Halen']['David Lee Roth']
+CORRECTED['Van Halen']['Sammy Hagar'] = ['vocals']
