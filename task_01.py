6faed395f929070e940c6c52b810e0953a3f479a
gpsingh12 <gurpreet_1441@yahoo.com>
2015-04-06 05:06:28 +0000

    GRADE_DATA

---
+++ task_01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Module 1"""
+
+
+GRADE_DATA = {
+    'Luke Skywalker': {
+        'math': 'B', 'etiquette': 'B+', 'grammar': 'B', 'gym': 'A'},
+    'Han Solo': {
+        'math': 'A-', 'etiquette': 'C-', 'grammar': 'B', 'gym': 'B'},
+    'C-3PO': {
+        'math': 'C', 'etiquette': 'A+', 'grammar': 'A', 'gym': 'F'}
+}
